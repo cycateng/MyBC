ae5af4b0ec47897e47d53434eba513263458017f
cycateng <cyber.gcw@gmail.com>
2018-06-05 18:16:50 +0900

    Block0を表示

--- MyBC.py
+++ MyBC.py
@@ -1,11 +1,26 @@
+from hashlib import sha256
+from datetime import datetime
+
 class Block:
     
     def __init__(self, index, previousHash, timestamp, data, hash):
         self.index = index
-        self.previousHash = previousHash
+        self.previousHash = str(previousHash)
         self.timestamp = timestamp
         self.data = data
-        self.hash = hash
+        self.hash = str(hash)
 
 def calculateHash(Block):
-    
+    pass
+
+index = 0
+initialHash = sha256(b"this is initial hash").hexdigest()
+timestamp = datetime.now()
+data = "block0data"
+previousHash = 0
+print("--------------------Block0---------------------")
+print(f"Index: {index}")
+print(f"Timestamp: {timestamp}")
+print(f"Data: {data}")
+print(f"Hash: {initialHash}")
+print(f"PreviousHash: {previousHash}")
