8e7bbc07f44c3f794d9ea4cf343ce89768c338de
cycateng <cyber.gcw@gmail.com>
2018-06-12 16:50:52 +0900

    hash計算関数を修正, ブロックチェーン検証機能を追加

--- MyBC.py
+++ MyBC.py
@@ -21,8 +21,52 @@ class Block:
         print()
 
     # ブロックのhashをメンバー変数から計算する
-    def calcBlockHash(self):
-        self.hash = str(sha256((str(self.index) + str(self.timestamp) + self.data + self.previousHash).encode()).hexdigest())
+#    def calcBlockHash(self):
+#        self.hash = str(sha256((str(self.index) + str(self.timestamp) + self.data + self.previousHash).encode()).hexdigest())
+
+# ブロックのハッシュを計算し返す機能
+# 引数にBlockのインスタンスを渡す
+def calcBlockHash(Block):
+    hash = sha256((str(Block.index) + str(Block.timestamp) + Block.data + Block.previousHash).encode()).hexdigest()
+    return str(hash)
+
+# 現在のブロックチェーンを出力
+def printBlockChain():
+    for i in range(9):
+        blockList[i].printBlockMember()
+
+# ブロックチェーン検証機能
+def checkBlockChain():
+    print("Now checking block chain...")
+    print()
+    print("format")
+    print("previousHash:")
+    print("hash:")
+    print()
+    for i in range(9)[::-1]:
+        if i == 0:
+            break
+        
+        #対象のブロックの1つ前のブロックのハッシュを計算し
+        #対象のブロックのpreviousHashと計算したハッシュを比較する
+        preHash = blockList[i].previousHash
+        hash = calcBlockHash(blockList[i-1])
+
+        print(f"Block{i}: {preHash}")
+        print(f"Block{i-1}: {hash}")
+        print()
+
+        if preHash != hash:
+            print("ブロックチェーンに矛盾を発見しました")
+            print(f"Contradiction: Block{blockList[i].index}'s previousHash and Block{blockList[i-1].index}'s hash")
+            print(f"preHash: {preHash}")
+            print(f"   hash: {hash}")
+            print()
+
+    print("Finish!")
+
+# Blockを格納するlist
+blockList = []
 
 # Block0を生成するための初期値
 index = 0
@@ -35,11 +79,23 @@ data = "block0data"
 # 各ブロックの変数dataはblock[ブロックのindex]というフォーマットにしている
 for i in range(9):
     Blocki = Block(index, previousHash, timestamp, data)
-    Blocki.calcBlockHash()
+    Blocki.hash = calcBlockHash(Blocki)
     Blocki.printBlockMember()
+    blockList.append(Blocki)
 
     # ブロック情報の更新
     index += 1
     timestamp += datetime.timedelta(seconds=10)
     data = f"block{index}data"
     previousHash = Blocki.hash
+
+print(blockList)
+print()
+
+#print("Block3のhashを改ざん")
+#blockList[3].hash = str(sha256("このブロックのハッシュを改ざんしてやったぜ!".encode()).hexdigest())
+print("Block6のtimestampを改ざん")
+blockList[6].timestamp = datetime.datetime.now()
+
+printBlockChain()
+checkBlockChain()
