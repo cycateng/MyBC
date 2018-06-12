ee1b3b2409c266f2ec492d7f7938498ce805064b
cycateng <cyber.gcw@gmail.com>
2018-06-12 14:28:35 +0900

    block生成とfor文でのテストまで

--- MyBC.py
+++ MyBC.py
@@ -1,37 +1,45 @@
 from hashlib import sha256
-from datetime import datetime
+import datetime
 
 class Block:
     
-    def __init__(self, index, previousHash, timestamp, data, hash):
+    def __init__(self, index, previousHash, timestamp, data):
         self.index = index
         self.previousHash = str(previousHash)
         self.timestamp = timestamp
         self.data = data
-        self.hash = str(hash)
+        self.hash = "empty"
 
+    # 生成されたブロックの情報を出力する
     def printBlockMember(self):
-        print("--------------------Block0---------------------")
-        print(f"Index: {index}")
-        print(f"Timestamp: {timestamp}")
-        print(f"Data: {data}")
-        print(f"Hash: {hash}")
-        print(f"PreviousHash: {previousHash}")
+        print(f"--------------------Block{self.index}---------------------")
+        print(f"Index: {self.index}")
+        print(f"Timestamp: {self.timestamp}")
+        print(f"Data: {self.data}")
+        print(f"Hash: {self.hash}")
+        print(f"PreviousHash: {self.previousHash}")
+        print()
 
-def calculateHash(Block):
-    pass
+    # ブロックのhashをメンバー変数から計算する
+    def calcBlockHash(self):
+        self.hash = str(sha256((str(self.index) + str(self.timestamp) + self.data + self.previousHash).encode()).hexdigest())
 
+# Block0を生成するための初期値
 index = 0
-initialHash = sha256(b"this is initial hash").hexdigest()
-timestamp = datetime.now()
-data = "block0data"
 previousHash = 0
-print("--------------------Block0---------------------")
-print(f"Index: {index}")
-print(f"Timestamp: {timestamp}")
-print(f"Data: {data}")
-print(f"Hash: {initialHash}")
-print(f"PreviousHash: {previousHash}")
-Block0 = Block(index, previousHash, timestamp, data, initialHash)
-print()
-Block0.printBlockMember()
+timestamp = datetime.datetime.now()
+data = "block0data"
+
+# forを0-8で回す
+# 各ブロックのタイムスタンプの時差は10秒にしている
+# 各ブロックの変数dataはblock[ブロックのindex]というフォーマットにしている
+for i in range(9):
+    Blocki = Block(index, previousHash, timestamp, data)
+    Blocki.calcBlockHash()
+    Blocki.printBlockMember()
+
+    # ブロック情報の更新
+    index += 1
+    timestamp += datetime.timedelta(seconds=10)
+    data = f"block{index}data"
+    previousHash = Blocki.hash
